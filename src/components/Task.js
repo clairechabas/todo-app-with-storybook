57d2984a0b6c193547b8e5e916a25f10adf6c527
Chabou <claire.chabas@gmail.com>
2019-09-27 11:55:42 +0200

    Updating HTML markup to match wanted design

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,8 +2,36 @@ import React from "react";
 
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+        ></span>
+      </label>
+
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input Title"
+        />
+      </div>
+
+      <div className="actions" onClick={e => e.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   );
 };
