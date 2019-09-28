cc6131d1d6f2348bb4a7bae24919f9a7a5630c2c
Chabou <claire.chabas@gmail.com>
2019-09-28 12:01:58 +0200

    Adding a story for the edge case where the title is too long and impact the task design

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Task from "./Task";
 
@@ -16,13 +17,22 @@ export const actions = {
   onArchiveTask: action("onArchiveTask")
 };
 
+// For the long title case
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
+
 // 1. To initiate Storybook we first call the storiesOf() function to register the component
 // 2. To define our stories, we call add() once for each of our test states to generate a story
 storiesOf("Task", module)
-  .add("default", () => <Task task={task} {...actions} />)
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    <Task task={object("task", { ...task })} {...actions} />
+  ))
   .add("pinned", () => (
     <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
   ))
   .add("archived", () => (
     <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  ))
+  .add("long title", () => (
+    <Task task={{ ...task, title: longTitle }} {...actions} />
   ));
